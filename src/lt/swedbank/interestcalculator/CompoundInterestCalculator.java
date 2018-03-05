e3e84cb6eedfc1f7fd95dc0adea2d2bdd834b639
karolisZuk <kar.zukas@gmail.com>
2018-03-06 01:36:28 +0200

    Update CompoundInterestCalculator.java
    Shit works strange.:(

--- src/lt/swedbank/interestcalculator/CompoundInterestCalculator.java
+++ src/lt/swedbank/interestcalculator/CompoundInterestCalculator.java
@@ -1,7 +1,7 @@
 package lt.swedbank.interestcalculator;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by p998zuh on 2018.03.05.
@@ -10,47 +10,50 @@ public class CompoundInterestCalculator {
 
     public static void main(String[] args) {
         double originalAmount;
-        double annualInterestRate;
-        double lengthOfTimeInterestIsApplied;
-        double oneYearTotal = 0;
-        char compoundingFrequency;
-        double compoundRecalc;
-        double interestAmounts[];
-
+        double annualInterestRate=1;
+        int lengthOfTimeInterestIsApplied;
+        double compoundingFrequency;
+        List<Double> tableOfInterestInputs = new ArrayList<Double>();
+        double[][] outputArray;
 
         Scanner inputScanner = new Scanner(System.in);
-        System.out.println("Please enter original (principal) amount ");
+        System.out.println("Amount: ");
         originalAmount = inputScanner.nextInt();
-        System.out.println("Please enter (annual) interest rate (%) ");
-        annualInterestRate = inputScanner.nextDouble();
-        System.out.println("Please enter overall length of time the interest is applied (in years) ");
-        lengthOfTimeInterestIsApplied = inputScanner.nextInt();
-        System.out.println("Please enter Compound frequency ");
-        compoundingFrequency = inputScanner.next().charAt(0);
 
-        compoundRecalc = (double) checkComputingFrequency(compoundingFrequency);
-        interestAmounts = new double[(int) lengthOfTimeInterestIsApplied * (int) compoundRecalc];
+        int arraySizeCounter=1;
+        double tempRatesTable[]=new double[1];
 
-        for (int i = 1; i <= interestAmounts.length; i++) {
-            double compoundIteration = (calculateIterativeCompoundInterest(originalAmount, annualInterestRate, i, compoundRecalc));
-            interestAmounts[i - 1] = Math.floor(compoundIteration * 100) / 100;
+        while (annualInterestRate!=0) {
+            System.out.println("Please enter (anual) interest rate (%) ");
+            annualInterestRate=inputScanner.nextDouble();
+            if(annualInterestRate==0||annualInterestRate>=100){
+                break;
+            }
+            tableOfInterestInputs.add(annualInterestRate);
         }
 
-        showOutput(annualInterestRate, (int) lengthOfTimeInterestIsApplied, originalAmount, oneYearTotal, compoundingFrequency, interestAmounts);
+        System.out.println("Please enter overall length of time the interest is applied (in years) ");
+        lengthOfTimeInterestIsApplied = inputScanner.nextInt();
 
+        System.out.println("Please enter Compound frequency ");
+        compoundingFrequency = checkComputingFrequency(inputScanner.next().charAt(0));
+
+        outputArray= new double[tableOfInterestInputs.size()][(int)(lengthOfTimeInterestIsApplied*compoundingFrequency)];
+
+        for (int i=0; i<tableOfInterestInputs.size(); i++) {
+            for(int x=1; x<=(int)(lengthOfTimeInterestIsApplied*compoundingFrequency); x++){
+                double percentDifference=calculateInterest(originalAmount, tableOfInterestInputs.get(i), compoundingFrequency, x);
+                outputArray[i][x-1]=calculatePercentDifference(percentDifference,originalAmount);
+            }
+        }
+
+        System.out.println(Arrays.deepToString(outputArray));
+
+        double compoundInterest=calculateInterest(originalAmount,tableOfInterestInputs.get(0), compoundingFrequency, lengthOfTimeInterestIsApplied);
     }
 
-    public static double calculateIterativeCompoundInterest(double amount, double interestRate, double lenghtOfTime, double compoundRecalc) {
-        return (amount * Math.pow((1 + ((interestRate / 100) / compoundRecalc)), (lenghtOfTime * compoundRecalc))) - amount - (interestRate * ((int) lenghtOfTime - 1));
-    }
-
-    public static void showOutput(double interestRate, int lengthOfTimeInterestIsApplied,
-                                  double originalAmount, double oneYearTotal, char compoundingFrequency, double[] interestAmounts) {
-        System.out.printf("Interest rate: " + "%.2f\n", interestRate);
-        System.out.println("Period length years: " + (int) lengthOfTimeInterestIsApplied);
-        System.out.printf("Total amount: " + "%.2f\n", (originalAmount + oneYearTotal));
-        System.out.printf("Compound frequency: " + compoundingFrequency + "\n");
-        System.out.println(Arrays.toString(interestAmounts));
+    public static double calculatePercentDifference(double difference, double oldValue){
+        return (difference/oldValue)*100;
     }
 
     public static int checkComputingFrequency(char validationString) {
@@ -68,6 +71,10 @@ public class CompoundInterestCalculator {
             default:
                 return 1;
         }
-
     }
+
+    public static double calculateInterest(double originalAmount, double interestRate,double compoundingFrequency, int years){
+        return originalAmount * Math.pow(1+((interestRate / 100)/compoundingFrequency),(compoundingFrequency*(double)years))-originalAmount;
+    }
+
 }
